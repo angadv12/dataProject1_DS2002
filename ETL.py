7e7f1068425209e767cead26ae70f9859b8c9141
Angad Brar <angadbrar@outlook.com>
2024-10-20 21:35:44 -0400

    initial commit, created pipeline file w/ outline

---
+++ ETL.py
@@ -0,0 +1,55 @@
+import requests
+import csv
+import json
+import pandas as pd
+import sqlite3
+
+
+"""
+1) fetch / download / retrieve data
+    - url
+    - local file
+"""
+# TODO
+
+
+"""
+2) detect format of file and convert data format
+    - input: csv or json or sql table
+    - output: file format of user choice
+"""
+# TODO
+
+
+"""
+3) modify number of columns of data
+    - reduce or add cols
+    - for add you can put any info you want
+"""
+# TODO
+
+
+"""
+4) store new file
+    - either to local files or sql database
+"""
+# TODO
+
+
+"""
+5) generate summary of data file ingestion
+    - include: num records, num cols
+"""
+# TODO
+
+
+"""
+6) generate summary of post processing
+    - include: num records, num cols
+"""
+# TODO
+
+
+# MAIN METHOD
+def process_data():
+    return;
